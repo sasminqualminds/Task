5c3c798e04f472066f0766fa026ad4dd0e8cdd74
sasminqualminds <sasmin@qualminds.com>
2024-04-26 13:41:06 +0530

    changes updated

--- StringReverse/Program.cs
+++ StringReverse/Program.cs
@@ -1,16 +1,22 @@
-﻿namespace StringReverse
+﻿
+// Reverse the string without using Inbuilt functions.
+
+namespace StringReverse
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Asking user to enter a string
             Console.WriteLine("Enter your string");
             string word=Console.ReadLine();
             
+            // looping the string from last to first
             for(int i=word.Length-1;i>=0; i--)
             {
-                Console.Write(word[i]+"");
+                Console.Write(word[i]);
             }
+            Console.ReadKey();
             
         }
     }
--- StringReverse/obj/Release/net8.0/StringReverse.AssemblyInfo.cs
+++ StringReverse/obj/Release/net8.0/StringReverse.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringReverse")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beaefdffccde9117c4cde8094a41bd6315eb41ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca4c4e0f5e3796a466e6c1e45853e16b5ec9996")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringReverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringReverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

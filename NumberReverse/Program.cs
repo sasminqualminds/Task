a63a8e528c82afa2c869f9217bba0973f23f17cc
sasminqualminds <sasmin@qualminds.com>
2024-05-01 13:40:22 +0530

    added comment

--- NumberReverse/Program.cs
+++ NumberReverse/Program.cs
@@ -10,7 +10,7 @@ namespace NumberReverse
             // Asking the user to enter a number
             Console.WriteLine("Enter your num");
             int number = Convert.ToInt32(Console.ReadLine());
-            string emptyString = "";
+            string empty = "";
 
             // checking condition if the number is zero
             if (number == 0){
@@ -21,12 +21,12 @@ namespace NumberReverse
             while (number != 0)                                 // checking if the number is not equal to 0
             {                              
                 int remainder = number % 10;                    // taking the remainder of that number which will be the last digit
-                emptyString = emptyString + remainder;          // adding that digit to an empty string
+                empty = empty + remainder;                      // adding that digit to an empty string
                 number = number / 10;                           // dividing the number so that the last digit will be removed
             }                                                   // checking the condition until the number is not equal to 0
             
             
-            Console.WriteLine("By reversing the number you get :" + emptyString);
+            Console.WriteLine("By reversing the number you get :" + empty);
             Console.ReadKey();
         }
     }
--- NumberReverse/obj/Debug/net8.0/NumberReverse.AssemblyInfo.cs
+++ NumberReverse/obj/Debug/net8.0/NumberReverse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c9ef09d06fe47b25bfa7e4c71d869dec5e6a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9510c624ec20ce9abf4741a84117c48988ba38f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

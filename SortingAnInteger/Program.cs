ee8418f4cb770f55209506b99adaccfe7a9b6cd4
sasminqualminds <sasmin@qualminds.com>
2024-05-01 11:42:27 +0530

    updated in TaskDevelopment

--- SortingAnInteger/Program.cs
+++ SortingAnInteger/Program.cs
@@ -11,35 +11,35 @@ namespace SortingAnInteger
             
             // Asking user to enter the length of the array
             Console.WriteLine("number of element you want to add into an array");
-            int numOfElements=Convert.ToInt32(Console.ReadLine());
-            int[] integerArray = new int[numOfElements];
+            int numberOfElements=Convert.ToInt32(Console.ReadLine());
+            int[] numberArray = new int[numberOfElements];
 
             // adding the elements to the array
-            for(int item =0; item < numOfElements; item++)
+            for(int item =0; item < numberOfElements; item++)
             {
                 Console.WriteLine($"Enter your {item+1} element");
                 
-                integerArray[item]= Convert.ToInt32(Console.ReadLine());
+                numberArray[item]= Convert.ToInt32(Console.ReadLine());
 
             }
 
             // printing the elements of array before sorting 
             Console.WriteLine("Array elements before sorting:");
-            foreach (int item in integerArray)
+            foreach (int item in numberArray)
             {               
                 Console.Write(item+" ");
             }
 
             // sorting the elements in ascending order
-            for (int item1 = 0; item1 < numOfElements-1; item1++)
+            for (int item1 = 0; item1 < numberOfElements-1; item1++)
             {
-                for(int item2 = 0; item2 < numOfElements-item1-1; item2++)
+                for(int item2 = 0; item2 < numberOfElements-item1-1; item2++)
                 {
-                    if (integerArray[item2] > integerArray[item2 + 1])
+                    if (numberArray[item2] > numberArray[item2 + 1])
                     {
-                        int temp = integerArray[item2];
-                        integerArray[item2]= integerArray[item2 + 1];
-                        integerArray[item2 + 1] = temp;
+                        int temp = numberArray[item2];
+                        numberArray[item2]= numberArray[item2 + 1];
+                        numberArray[item2 + 1] = temp;
                     }
                 }
             }
@@ -47,7 +47,7 @@ namespace SortingAnInteger
 
             // printing the elements in array after sorting
             Console.WriteLine("Array elements after sorting:");
-            foreach(int element in integerArray)
+            foreach(int element in numberArray)
             {
                 Console.Write(element + " ");
             }

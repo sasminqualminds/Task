7f34c3f5582dbe918190e8bdfc3720a439325db9
sasminqualminds <sasmin@qualminds.com>
2024-05-02 12:26:29 +0530

    changed variable name

--- NumberReverse/obj/Debug/net8.0/NumberReverse.AssemblyInfo.cs
+++ NumberReverse/obj/Debug/net8.0/NumberReverse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9510c624ec20ce9abf4741a84117c48988ba38f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e073ec411e530bef165919283f0ebfa02f8d546d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberReverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

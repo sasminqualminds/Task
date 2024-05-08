593f61f0a5eaf441d95ea87aa41b9f804b77984e
sasminqualminds <sasmin@qualminds.com>
2024-05-08 12:33:27 +0530

    Arithmetic Operations using delegates

---
+++ ArithmeticOperationsUsingDelegates/InputOutput.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperationsUsingDelegates
+{
+    internal class InputOutput
+    {
+        Operations operations=new Operations();
+        public delegate int ArithmeticOperationDelegate(int a, int b);                     // Declaring a delegate
+
+        public void add()
+        {
+            Console.WriteLine("Enter your first number");                                   // Asking user to enter first number
+            int firstNumber=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");                                  // Asking user to enter second number
+            int secondNumber = int.Parse(Console.ReadLine());
+            ArithmeticOperationDelegate arithmeticOperationDelegate = new ArithmeticOperationDelegate(operations.add);      //It points to the add method in Operations class
+            int sum= arithmeticOperationDelegate(firstNumber, secondNumber);                
+
+            Console.WriteLine($"Addition of {firstNumber} and {secondNumber} is {sum}");     // Prints the output
+
+        }
+        public void subtract()
+        {
+            Console.WriteLine("Enter your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            ArithmeticOperationDelegate arithmeticOperationDelegate = new ArithmeticOperationDelegate(operations.subtract);
+            int subtraction = arithmeticOperationDelegate(firstNumber, secondNumber);
+
+            Console.WriteLine($"Subtraction of {firstNumber} and {secondNumber} is {subtraction}");
+
+        }
+        public void multiply()
+        {
+            Console.WriteLine("Enter your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            ArithmeticOperationDelegate arithmeticOperationDelegate = new ArithmeticOperationDelegate(operations.multiply);
+            int multiplication = arithmeticOperationDelegate(firstNumber, secondNumber);
+
+            Console.WriteLine($"Multiplication of {firstNumber} and {secondNumber} is {multiplication}");
+
+        }
+
+        public void divide()
+        {
+            Console.WriteLine("Enter your first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            ArithmeticOperationDelegate arithmeticOperationDelegate = new ArithmeticOperationDelegate(operations.divide);
+            int division = arithmeticOperationDelegate(firstNumber, secondNumber);
+
+            Console.WriteLine($"Division of {firstNumber} and {secondNumber} is {division}");
+
+        }
+    }
+}
---
+++ ArithmeticOperationsUsingDelegates/Operations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperationsUsingDelegates
+{
+    internal class Operations
+    {
+        
+        // calculates addition of two numbers and returns the value
+        public int add(int firstNumber,int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        // calculates subtraction of two numbers and returns the value
+        public int subtract(int firstNumber,int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        // performs multiplication operation on two numbers and returns the value
+        public int multiply(int firstNumber,int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        // performs division operation on two numbers and returns the value
+        public int divide(int firstNumber,int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}

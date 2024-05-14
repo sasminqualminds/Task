13da20360869ddc050eb1eb1649d350c7c338e01
sasminqualminds <sasmin@qualminds.com>
2024-05-14 18:15:02 +0530

    Overriding

---
+++ Overriding/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    // Derived class inheriting from Animal
+    internal class Cat:Animal
+    {
+        // Overriding the MakeSound method
+        public override void MakesSound()
+        {
+            Console.WriteLine("Cat makes sound meow");
+        }
+
+        // Overriding the Move method
+        public override void Move()
+        {
+            Console.WriteLine("cat jumps");
+        }
+
+        // Overriding the Eat method
+        public override void Eat()
+        {
+            Console.WriteLine("cat eats fish");
+        }
+    }
+}
---
+++ Overriding/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Write a Program to Demonstrate the Overriding
+
+namespace Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            // Calling MakeSound, Move, and Eat methods of base class
+            animal.MakesSound();
+            animal.Move();
+            animal.Eat();
+            Console.WriteLine();
+
+            // Calling MakeSound, Move, and Eat methods of derived classes
+            dog.MakesSound();
+            dog.Eat();
+            dog.Move();
+            Console.WriteLine();
+
+            cat.MakesSound();
+            cat.Eat();
+            cat.Move();
+            Console.WriteLine();
+        }
+    }
+}

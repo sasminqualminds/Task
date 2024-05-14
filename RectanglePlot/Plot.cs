988309e9de0d541be4fa5ac1d10cd7c2bfc6c499
sasminqualminds <sasmin@qualminds.com>
2024-05-14 18:14:34 +0530

    RectanglePlot

---
+++ RectanglePlot/Plot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectanglePlot
+{
+    // inherited the fields and methods of Rectangle plot
+    internal class Plot:Rectangle
+    {
+        // field
+        public double cost;
+
+        // method
+        public double costOfRectanglePlot()
+        {
+            return areaOfRectangle()*cost;           
+        }
+    }
+}
---
+++ RectanglePlot/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// Write a Program to Display Cost of a Rectangle Plot Using Inheritance
+
+namespace RectanglePlot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Plot plot = new Plot();
+            
+            // Asking the user to enter the length of the rectangle
+            Console.WriteLine("Enter length of rectangle");
+            plot.length =double.Parse(Console.ReadLine());
+
+            // Asking the user to enter the width of the rectangle
+            Console.WriteLine("Enter breadth of rectangle");
+            plot.breadth = double.Parse(Console.ReadLine());
+
+            // Asking the user to enter the cost per square unit
+            Console.WriteLine("Enter cost per sqaure unit");
+            plot.cost = double.Parse(Console.ReadLine());
+            
+            // Printing the cost of the plot
+            Console.WriteLine("Cost of the plot: $"+plot.costOfRectanglePlot());
+        }
+    }
+}

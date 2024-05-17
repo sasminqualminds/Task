6d0a6a84e312cf2ec5b9bf7464b54a0d637141cf
sasminqualminds <sasmin@qualminds.com>
2024-05-17 19:56:39 +0530

    EmployeeAPI

---
+++ EmployeeApi/Data/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Data
+{
+    // Represents the database context for the Employee entity
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> contextOptions) : base(contextOptions)
+        { }
+
+        // DbSet representing the collection of Employee entities in the database
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeApi/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿
+// Create employee API, Create Controller, create employee table in SQL,
+// Perform CRUD operations using swagger
+// employee-table(employeeID(pk),first name, second name, salary, department name, phone number)  
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeApi.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+        public string DepartmentName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
